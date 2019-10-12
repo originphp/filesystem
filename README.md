2e774560fa30caa0d7b53d930608b8d34d05fcfe
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-12 12:11:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,332 @@
+# Filesystem
+
+![license](https://img.shields.io/badge/license-MIT-brightGreen.svg)
+[![build](https://travis-ci.org/originphp/filesystem.svg?branch=master)](https://travis-ci.org/originphp/filesystem)
+[![coverage](https://coveralls.io/repos/github/originphp/filesystem/badge.svg?branch=master)](https://coveralls.io/github/originphp/filesystem?branch=master)
+
+Filesystem includes the `File` and `Folder` classes for working with your filesystem.
+
+## Installation
+
+To install this package
+
+```linux
+$ composer require originphp/filesystem
+```
+
+
+## File
+
+he file utility wraps some important functions in an easy to use and predictable way.
+
+To use the File utility add the following to the top of your file.
+
+```php
+use Origin\Filesystem\File
+```
+
+### Info
+
+To get information about a file
+
+```php
+$info = File::info('/var/www/config/insurance.csv');
+```
+
+Which will return this
+
+```
+Array
+(
+    [name] => insurance.csv
+    [path] => /var/www/config
+    [extension] => csv
+    [type] => text/plain
+    [size] => 1878
+    [timestamp] => 1560067334
+)
+```
+
+### Read
+
+To read a file
+
+```php
+$contents = File::read('/path/somefile');
+```
+
+### Write
+
+To write to a file
+
+
+```php
+File::write('/path/somefile','data goes here');
+```
+
+### Append
+
+To append contents to a file
+
+```php
+File::append('/path/somefile','and here.');
+```
+
+### Delete
+
+To delete a file
+
+```php
+File::delete('/path/somefile');
+```
+
+### Exists
+
+To check if file exists
+
+```php
+$result = File::exists('/path/somefile');
+```
+
+### Tmp
+
+When needing to work with temporary files, use tmp, this will create the file, put the contents and return to you the name of the file with path.
+
+```php
+$tmpFile = File::tmp('Some temp data');
+```
+
+### Copy
+
+To copy a file
+
+```php
+File::copy('/path/somefile','somefile-backup');
+File::copy('/path/somefile','/another_path/somefile');
+```
+
+### Rename
+
+To rename a file
+
+```php
+File::rename('/path/somefile','new_name');
+```
+
+### Move
+
+To move a file
+
+```php
+File::move('/path/somefile','/another_path/somefile');
+```
+
+### Permissions
+
+#### Get Permissions
+
+To get the permissions of a file
+
+```php
+$permissions = File::permissions('/path/somefile'); // returns 0744
+```
+
+#### Changing Permissions (chmod)
+
+To change the permissions of a file.
+
+```php
+File::chmod('/path/somefile','www-data');
+```
+
+#### Getting the owner of the file
+
+```php
+$owner = File::owner('/path/somefile'); // returns root
+```
+
+#### Changing Ownership (chown)
+
+To change the ownership of a file
+
+```php
+File::chown('/path/somefile','www-data');
+```
+
+#### Getting the group
+
+To get the group that the file belongs to.
+
+```php
+$group = File::group('/path/somefile'); // returns root
+```
+
+#### Changing Group (chgrp)
+
+To change the group that the file belongs to.
+
+```php
+File::chgrp('/path/somefile','www-data');
+```
+
+## Folder
+
+The folder utility helps you work with folders on your file system.
+
+To use the Folder utility add the following to the top of your file.
+
+```php
+use Origin\Filesystem\Folder
+```
+
+### Create
+
+To create a folder
+
+```php
+Folder::create('/var/www/new_folder');
+```
+
+To create a folder recursively
+
+```php
+Folder::create('/var/www/level1/level2/level3/new_folder',['recursive'=>true]);
+```
+
+To set the permissions on the newly created folder
+
+```php
+Folder::create('/var/www/new_folder',['mode'=>0755]);
+```
+
+### Delete
+
+To delete a folder
+
+```php
+Folder::delete('/var/www/bye-bye')
+```
+
+To delete a folder recursively, including all files and sub directories.
+
+```php
+Folder::delete('/var/www/docs',['recursive'=>true])
+```
+
+### Exists
+
+To check if a directory exists
+
+```php
+$result = Folder::exists('/path/somedirectory');
+```
+
+### List
+
+To list all contents of a directory
+
+```php
+$results = Folder::list('/path/somedirectory');
+```
+
+This will return an array of arrays of files, like this
+
+```php
+[
+    [
+        'name' => 'foo.txt',
+        'path' => '/var/www/my_directory'
+        'size' => 1234,
+        'timestamp' => 14324234,
+        'type' => 'file'
+    ]
+]
+```
+
+You can also get the listing recursively
+
+```php
+$results = Folder::list('/path/somedirectory',['recursive'=>true]);
+```
+
+To include directories in the results
+
+```php
+$results = Folder::list('/path/somedirectory',['directories'=>true]);
+```
+
+### Copy
+
+To copy a directory
+
+```php
+Folder::copy('/path/somedir','somedir-backup');
+Folder::copy('/path/somedir','/another_path/somedir');
+```
+
+### Rename
+
+To rename a directory
+
+```php
+Folder::rename('/path/somedir','new_name');
+```
+
+### Move
+
+To move a directory
+
+```php
+Folder::move('/path/somedir','/another_path/somedir');
+```
+
+### Permissions
+
+#### Get Permissions
+
+To get the permissions of a directory.
+
+```php
+$permissions = Folder::mode('/path/somedir'); // returns 0744
+```
+
+#### Changing Permissions (chmod)
+
+To change the permissions of a directory
+
+```php
+Folder::chmod('/path/somedir','www-data');
+Folder::chmod('/path/somedir','www-data',['recursive'=>true]); // recursive
+```
+
+#### Getting the owner of a directory
+
+```php
+$owner = Folder::owner('/path/somedir'); // returns root
+```
+
+#### Changing Ownership (chown)
+
+To change the ownership of a directory
+
+```php
+Folder::chown('/path/somedir','www-data');
+Folder::chown('/path/somedir','www-data',['recursive'=>true]);
+```
+
+#### Getting the group
+
+To get the group that a directory belongs to.
+
+```php
+$group = Folder::group('/path/somedir'); // returns root
+```
+
+#### Changing Group (chgrp)
+
+To change the group that the folder belongs to.
+
+```php
+Folder::chgrp('/path/somedir','www-data');
+Folder::chgrp('/path/somedir','www-data',['recursive'=>true]);
+```
---
+++ src/Folder.php
@@ -0,0 +1,325 @@
+<?php
+declare(strict_types = 1);
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Filesystem;
+
+use Origin\Filesystem\Exception\NotFoundException;
+
+class Folder
+{
+    /**
+     * Creates a folder or folders recursively
+     *
+     * @param string $directory
+     * @param array $options
+     *  - recursive: default false. like -p creates all directories in one go
+     *  - mode: default 0775. Folder permissions
+     * @return boolean
+     */
+    public static function create(string $directory, array $options = []) : bool
+    {
+        $options += ['recursive' => false,'mode' => 0775];
+        defer($a, 'umask', umask(0));
+        return @mkdir($directory, $options['mode'], $options['recursive']); # use@ No such file or directory
+    }
+
+    /**
+     * Checks if a directory exists
+     *
+     * @param string $directory
+     * @return boolean
+     */
+    public static function exists(string $directory) : bool
+    {
+        return file_exists($directory) and is_dir($directory);
+    }
+
+    /**
+     * Includes a list of files
+     *
+     * @param string $directory
+     * @param array $options Options keys are
+     *  - directories: default false, includes directories
+     *  - recursive: default false. Recursively gets contents
+     * @return array
+     */
+    public static function list(string $directory, array $options = []) : array
+    {
+        $options += ['directories' => false,'recursive' => false];
+        if (self::exists($directory)) {
+            $results = [];
+            $files = array_diff(scandir($directory), ['.', '..']);
+            foreach ($files as $file) {
+                if ($options['recursive'] and ! is_file($directory . DIRECTORY_SEPARATOR . $file)) {
+                    $results = array_merge($results, static::list($directory . DIRECTORY_SEPARATOR . $file, $options));
+                }
+                if (! $options['directories'] and ! is_file($directory . DIRECTORY_SEPARATOR . $file)) {
+                    continue;
+                }
+                $stats = stat($directory . DIRECTORY_SEPARATOR . $file);
+                $results[] = [
+                    'name' => $file,
+                    'path' => $directory,
+                    'timestamp' => $stats['mtime'],
+                    'size' => $stats['size'],
+                    'type' => is_dir($directory. DIRECTORY_SEPARATOR . $file)?'directory':'file',
+                ];
+            }
+
+            return $results;
+        }
+       
+        throw new NotFoundException(sprintf('%s does not exist', $directory));
+    }
+
+    /**
+     * Deletes a directory. For saftey reasons recrusive is disabled by default,
+     * since this will delete files etc.
+     *
+     * @param string $directory
+     * @param array $options These are the options can you use:
+     *    - recursive: If set to true, it will delete all contents and sub folders
+     * @return bool
+     */
+    public static function delete(string $directory, array $options = []) : bool
+    {
+        $options += ['recursive' => false];
+        if (self::exists($directory)) {
+            if ($options['recursive']) {
+                $files = array_diff(scandir($directory), ['.', '..']);
+                foreach ($files as $filename) {
+                    if (is_dir($directory . DIRECTORY_SEPARATOR . $filename)) {
+                        self::delete($directory . DIRECTORY_SEPARATOR . $filename, $options);
+                        continue;
+                    }
+                    @unlink($directory . DIRECTORY_SEPARATOR . $filename);
+                }
+            }
+
+            return @rmdir($directory);
+        }
+        
+        throw new NotFoundException(sprintf('%s does not exist', $directory));
+    }
+
+    /**
+     * Renames a directory
+     *
+     * @param string $directory full patth e.g. /var/www/tmp/my_project
+     * @param string $to  directory name. project_name
+     * @return boolean
+     */
+    public static function rename(string $directory, string $to) : bool
+    {
+        if (self::exists($directory)) {
+            if (strpos($to, DIRECTORY_SEPARATOR) === false) {
+                $to = pathinfo($directory, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . $to;
+            }
+
+            return @rename($directory, $to);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+     * Moves a directory
+     *
+     * @param string $source /var/www/tmp/docs
+     * @param string $destination /var/www/tmp/documents
+     * @return bool
+     */
+    public static function move(string $source, string $destination) : bool
+    {
+        if (self::exists($source)) {
+            if (strpos($destination, DIRECTORY_SEPARATOR) === false) {
+                $destination = pathinfo($source, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . $destination;
+            }
+
+            return @rename($source, $destination);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $source));
+    }
+
+    /**
+     * Copies a directory
+     *
+     * @param string $source /var/www/tmp/my_project
+     * @param string $destination project_name or /var/www/tmp/project_name
+     * @return boolean
+     */
+    public static function copy(string $source, string $destination) : bool
+    {
+        if (self::exists($source)) {
+            if (strpos($destination, DIRECTORY_SEPARATOR) === false) {
+                $destination = pathinfo($source, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . $destination;
+            }
+
+            @mkdir($destination);
+
+            $files = array_diff(scandir($source), ['.', '..']);
+            foreach ($files as $filename) {
+                if (is_dir($source . DIRECTORY_SEPARATOR . $filename)) {
+                    self::copy($source . DIRECTORY_SEPARATOR . $filename, $destination . DIRECTORY_SEPARATOR . $filename);
+                    continue;
+                }
+                @copy($source . DIRECTORY_SEPARATOR . $filename, $destination . DIRECTORY_SEPARATOR . $filename);
+            }
+
+            return self::exists($destination);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $source));
+    }
+
+    /**
+    * Gets or sets the permissions (mode) for a directory
+    *
+    * @param string $directory filename with full path
+    * @return string
+    */
+    public static function mode(string $directory) : string
+    {
+        if (self::exists($directory)) {
+            return  (string) substr(sprintf('%o', fileperms($directory)), -4);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+    * Alias for mode. Gets the mode for a file aka permissions
+    *
+    * @param string $directory
+    * @return string|null
+    */
+    public static function perms(string $directory) : ?string
+    {
+        return static::mode($directory);
+    }
+
+    /**
+      * Gets the owner of directory
+      *
+      * @param string $directory filename with full path
+      * @return string|null
+      */
+    public static function owner(string $directory) : ?string
+    {
+        if (self::exists($directory)) {
+            return posix_getpwuid(fileowner($directory))['name'];
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+     * Gets the group that the directory belongs to.
+     *
+     * @param string $directory filename with full path
+     * @return string|null
+     */
+    public static function group(string $directory) : ?string
+    {
+        if (self::exists($directory)) {
+            return posix_getgrgid(filegroup($directory))['name'];
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+    * Changes the permissions of directory
+    *
+    * @param string $directory filename with full path
+    * @param int $mode e.g 0755 (remember 0 infront)
+     * @param array $options (recursive default false)
+     *  - recursive: If set to true, it will delete all contents and sub folders
+    * @return bool
+    */
+    public static function chmod(string $directory, int $mode, array $options = []) : bool
+    {
+        $options += ['recursive' => false];
+
+        if (self::exists($directory)) {
+            if ($options['recursive']) {
+                $files = array_diff(scandir($directory), ['.', '..']);
+                foreach ($files as $filename) {
+                    if (is_dir($directory . DIRECTORY_SEPARATOR . $filename)) {
+                        self::chmod($directory . DIRECTORY_SEPARATOR . $filename, $mode, $options);
+                        continue;
+                    }
+                    @chmod($directory . DIRECTORY_SEPARATOR . $filename, $mode);
+                }
+            }
+
+            return @chmod($directory, $mode);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+     * Changes the owner of the directory
+     *
+     * @param string $directory filename with full path
+     * @param string $user  e.g. root, www-data
+     * @param array $options Support options keys are
+     *  - recursive: default false. If set to true, it will delete all contents and sub folders
+     * @return bool
+     */
+    public static function chown(string $directory, string $user, array $options = []) : bool
+    {
+        $options += ['recursive' => false];
+        if (self::exists($directory)) {
+            if ($options['recursive']) {
+                $files = array_diff(scandir($directory), ['.', '..']);
+                foreach ($files as $filename) {
+                    if (is_dir($directory . DIRECTORY_SEPARATOR . $filename)) {
+                        self::chown($directory . DIRECTORY_SEPARATOR . $filename, $user, $options);
+                        continue;
+                    }
+                    @chown($directory . DIRECTORY_SEPARATOR . $filename, $user);
+                }
+            }
+
+            return @chown($directory, $user);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+
+    /**
+    * Changes the group that the directory belongs to
+    *
+    * @param string $directory filename with full path
+    * @param string $group  e.g. root, www-data
+    * @param array $options (recursive default false)
+    *  - recursive: If set to true, it will delete all contents and sub folders
+    * @return bool
+    */
+    public static function chgrp(string $directory, string $group = null, array $options = []) : bool
+    {
+        $options += ['recursive' => false];
+        if (self::exists($directory)) {
+            if ($options['recursive']) {
+                $files = array_diff(scandir($directory), ['.', '..']);
+                foreach ($files as $filename) {
+                    if (is_dir($directory . DIRECTORY_SEPARATOR . $filename)) {
+                        self::chgrp($directory . DIRECTORY_SEPARATOR . $filename, $group, $options);
+                        continue;
+                    }
+                    @chgrp($directory . DIRECTORY_SEPARATOR . $filename, $group);
+                }
+            }
+
+            return @chgrp($directory, $group);
+        }
+        throw new NotFoundException(sprintf('%s could not be found', $directory));
+    }
+}
